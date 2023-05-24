6f75ba706be5e38bcc1abde99f212e2c60f6f093
Christer Carlsson <ccarlsson@outlook.com>
2023-05-24 15:19:04 +0200

    BGP-11: Connect to Firebase from API

--- Bugporter.API/Startup.cs
+++ Bugporter.API/Startup.cs
@@ -1,4 +1,6 @@
 ﻿using Bugporter.API.Features.ReportBug.GitHub;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,19 +14,31 @@ public class Startup : FunctionsStartup
     public override void Configure(IFunctionsHostBuilder builder)
     {
         var config = builder.GetContext().Configuration;
-        builder.Services.Configure<GitHubRepositoryOptions>(options =>
+        var services = builder.Services;
+
+        services.Configure<GitHubRepositoryOptions>(options =>
         {
             options.Owner = config.GetValue<string>("GITHUB_REPOSITORY_OWNER");
             options.Name = config.GetValue<string>("GITHUB_REPOSITORY_NAME");
         });
 
+        var firebaseConfig = config.GetValue<string>("FIREBASE_CONFIG");
+          
+        var firebaseApp = FirebaseApp.Create(new AppOptions
+        {
+            Credential = GoogleCredential.FromJson(firebaseConfig)
+        });
+
+
+        services.AddSingleton(firebaseApp);
+
         var githubToken = config.GetValue<string>("GITHUB_TOKEN");
 
-        builder.Services.AddSingleton(new GitHubClient(new ProductHeaderValue("bugporter-api"))
+        services.AddSingleton(new GitHubClient(new ProductHeaderValue("bugporter-api"))
         {
             Credentials = new Credentials(githubToken)
         });
 
-        builder.Services.AddSingleton<CreateGitHubIssueCommand>();
+        services.AddSingleton<CreateGitHubIssueCommand>();
     }
 }
